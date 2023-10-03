bdb0c3881e0f8f4078f70b66c61b673babeb6c7a
katongole <katsisaac50@gmail.com>
2023-10-04 00:30:27 +0200

    adding form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Learning React</h1>
+    <div className="Container">
+     <h1 className='display-4 '>Learning React</h1>
+     <br/>
+     <div className='row'>
+     <div className='col-md-6 offset-3'>
      <form>
-     <input>
-     </input>
-     <input>
-     </input>
-     <button>
+     <div className='mb-3'>
+     <input type="text" className='form-control'/>
+     </div>
+     
+     <div className='mb-3'>
+     <input type="password" className='form-control'/>
+     </div>
+     <button className='btn btn-primary'> Submit
      </button>
      </form>
+     </div>
+     </div>
+
+     
     </div>
   );
 }
