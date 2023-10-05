1d6ea118575536f1b1eaf804f8fd9905898f15b2
katongole <katsisaac50@gmail.com>
2023-10-05 20:25:59 +0200

    implement fetch to get data from an api

--- src/App.js
+++ src/App.js
@@ -3,44 +3,71 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState('');
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(email, password);
-}
-
-  return (
+  const fetchUsersData = () => {
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }
+  return(
     <div className="Container">
-     <h1 className='display-4 '>Learning React</h1>
-     <br/>
-     <div className='row'>
-     <div className='col-md-6 offset-3'>
-     <form onSubmit={handleSubmit}>
-     <div className='mb-3'>
-     <label className='form-label'>Enter Email</label>
-     <input autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} 
-     type="text" className='form-control'
-     />
-     <div className='form-text'>
-     we'll never share your email with anyone else.
-     </div>
-     </div>
-     
-     <div className='mb-3'>
-     <label className='form-label'>Enter Password</label>
-     <input autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)} type="password" className='form-control'/>
-     </div>
-     <button className='btn btn-primary'> Submit
-     </button>
-     </form>
-     </div>
-     </div>
-
-     
+    {users.map((user) => (
+      <div className='col-md-6 offset-3' key={user.id}>
+      <h1 className='display-4 '>{user.name}</h1>
+      </div>
+      
+    ))}
+    {console.log(users)}
+    <button onClick={fetchUsersData}>Fetch Users</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// function App() {
+// const [email, setEmail] = useState('');
+// const [password, setPassword] = useState('');
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   console.log(email, password);
+// }
+
+//   return (
+//     <div className="Container">
+//      <h1 className='display-4 '>Learning React</h1>
+//      <br/>
+//      <div className='row'>
+//      <div className='col-md-6 offset-3'>
+//      <form onSubmit={handleSubmit}>
+//      <div className='mb-3'>
+//      <label className='form-label'>Enter Email</label>
+//      <input autoComplete="email" value={em ail} onChange={(e) => setEmail(e.target.value)} 
+//      type="text" className='form-control'
+//      />
+//      <div className='form-text'>
+//      we'll never share your email with anyone else.
+//      </div>
+//      </div>
+     
+//      <div className='mb-3'>
+//      <label className='form-label'>Enter Password</label>
+//      <input autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)} type="password" className='form-control'/>
+//      </div>
+//      <button className='btn btn-primary'> Submit
+//      </button>
+//      </form>
+//      </div>
+//      </div>
+
+     
+//     </div>
+//   );
+// }
+
+// export default App;
